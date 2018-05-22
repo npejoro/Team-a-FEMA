18b85dd0cf57c10c7149f10fe48b551631dbdaa9
ericalei <ericalei@berkeley.edu>
2018-05-21 22:33:09 -0700

    added app.py updates

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, jsonify, send_from_directory, redirect
+from flask import Flask, render_template, jsonify, send_from_directory, request, redirect
 from flask_pymongo import PyMongo
 import scrape_fema
-
 from flask_assets import Bundle, Environment
+
+import os
+import pandas as import pd
+import numpy as np
 
 app = Flask(__name__, static_folder='static')
 mongo = PyMongo(app)
@@ -34,3 +37,43 @@ def scrape():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# victoria
+df = pd.read_csv("noaadata.csv")
+Cost = df["Cost"].tolist()
+Deaths = df["Deaths"].tolist()
+Name = df["Name"].tolist()
+Year = df["Year"].tolist()
+Disaster = df["Disaster"].tolist()
+disasterType = df["disasterType"].tolist()
+impactScale = df["impactScale"].tolist()
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+
+@app.route("/future")
+def future():
+    data=[{
+        "Cost": Cost,
+        "Deaths":Deaths,
+        "Name": Name,
+        "Year": Year,
+        "Disaster": Disaster,
+        "disasterType": disasterType,
+        "impactScale": impactScale,
+    }]
+
+    for i in (data[0]['Deaths']):
+        # print (type(i))
+        if not isinstance(i,int):
+            print (i)
+        # print(data[0]['Deaths'])
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+     app.run(debug=True)
