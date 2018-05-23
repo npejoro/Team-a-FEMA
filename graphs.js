d4ec8ed13f5e3a078698ad64e5e1323a697d114c
Nicole L <nicolenlove21@gmail.com>
2018-05-22 18:22:08 -0700

    disaster filter instead of region

--- graphs.js
+++ graphs.js
@@ -1,8 +1,8 @@
-var regions = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+var disasters = ['Other', 'Earthquake', 'Fire', 'Flood', 'Human', 'Hurricane', 'Snow', 'Tornado', 'Drought']
 
 Plotly.d3.csv('https://raw.githubusercontent.com/npejoro/Team-a-FEMA/nl_branch/FEMA4.csv', (err, rows) => {
-    var data = regions.map(y => {
-        var d = rows.filter(r => r.Region === y)
+    var data = disasters.map(y => {
+        var d = rows.filter(r => r.IncidentType === y)
         console.log(d)
         return {
             mode: 'markers',
@@ -18,11 +18,10 @@ Plotly.d3.csv('https://raw.githubusercontent.com/npejoro/Team-a-FEMA/nl_branch/F
             y: d.map(r => r.Count)
         }
     })
-
     Plotly.newPlot('graph', data)
     
     var update = {
-        width: 1200,
+        width: 1350,
         height:700
     }
     Plotly.relayout('graph', update)
