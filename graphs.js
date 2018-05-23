167be6a92617b2d86ecf69869fed5861639d715d
Nicole L <nicolenlove21@gmail.com>
2018-05-22 17:37:52 -0700

    updates 5/22

--- graphs.js
+++ graphs.js
@@ -18,20 +18,7 @@ Plotly.d3.csv('https://raw.githubusercontent.com/npejoro/Team-a-FEMA/nl_branch/F
             y: d.map(r => r.Count)
         }
     })
-//    var layout = {
-//        autosize: false,
-//        width: 500,
-//        height: 1000,
-//        margin: {
-//            l: 50,
-//            r: 50,
-//            b: 100,
-//            t: 100,
-//            pad: 4
-//        },
-//        paper_bgcolor: '#7f7f7f',
-//        plot_bgcolor: '#c7c7c7'
-//    };
+
     Plotly.newPlot('graph', data)
     
     var update = {
